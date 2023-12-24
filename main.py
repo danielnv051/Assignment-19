ec88411c1f46da7dc58ca29f45393b0137725c31
danielnv051 <danielnv0910@gmail.com>
2023-12-24 21:46:21 +0330

    complete assignment19

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
-from ui_mainwindow import Ui_MainWindow
+from mainwindow import Ui_mainwindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()
+        self.ui = Ui_mainwindow()
         self.ui.setupUi(self)
 
-main_window = MainWindow()
 app = QApplication(sys.argv)
 
+main_window = MainWindow()
 main_window.show()
 
 app.exec()
